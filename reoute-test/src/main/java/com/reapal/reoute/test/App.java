4b89e91cd00925ebf73e61dc312669e94f9b6426
hyacker <chpe00@sina.com>
2014-12-09 17:19:23 +0800

    test

---
+++ reoute-test/src/main/java/com/reapal/reoute/test/App.java
@@ -0,0 +1,13 @@
+package com.reapal.reoute.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
